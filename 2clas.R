16de474c57dd484d6992785fa135a82e2ee228ce
Corina Lazo <lazocorinafanny123@gmail.com>
2020-06-06 17:57:30 -0500

    poniendome al dia  1 clase

---
+++ 2clas.R
@@ -0,0 +1,11 @@
+setwd(dir = "C:/Users/DANIPZA/Desktop/pylinR/Rclass")
+#definicion de matrices 
+help("matrix")
+mat1 <- matrix(data = 1:9, nrow = 3,ncol = 3)
+mat2 <- matrix(data = 1:9, ncol = 3)
+mat3 <- matrix(data = 1:15, nrow = 5)
+#byrow= FALSE : 
+
+
+
+#din(mat3) nos da un vector 
---
+++ ejemplo2.R
@@ -0,0 +1,17 @@
+getwd()
+setwd("C:/Users/DANIPZA/Desktop/pylinR/Rclass")
+dir.create("corian")
+dir()
+help("log")
+log(8,base = 2)
+vec1 <- 1:5
+vec2 <- c(1,4,5,5.7)
+vec3 <- c(vec1,vec2)
+help("rnorm")
+vect4 <- rnorm(5,mean = 5,sd = 2)
+vect5 <- sum(vect4)
+vect5 <- vect5/5
+help("runif")
+vec6 <- runif(5,min = 4,max = 7)
+help("rchisq")
+help("seq")
